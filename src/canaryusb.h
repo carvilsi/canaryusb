ffc2a02941875b5ced94dfff270b8833b0c0c28e
carvilsi <carvilsi.mail.list@gmail.com>
2025-10-29 15:06:33 +0100

    Fixes a bug related with command line args parsing and wrong free after malloc.

--- src/canaryusb.h
+++ src/canaryusb.h
@@ -55,7 +55,7 @@
 #endif                                   
 
 #define NAME    "canaryusb" 
-#define VERSION "5.0.0"
+#define VERSION "5.0.1"
 
 #define BOLD_TEXT    "\e[1m"
 #define NO_BOLD_TEXT "\e[m"
